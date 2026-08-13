a296c5f67dbb2b10d4ee21d95fbd3846a1a1689e
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-14 05:23:03 +0800

    refactor: remove leading slashes from API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,15 +5,15 @@ use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('secret.token')->group(function () {
-	Route::post('/register', [AuthController::class, 'register']);
-	Route::post('/login', [AuthController::class, 'login'])->name('login');
+	Route::post('register', [AuthController::class, 'register']);
+	Route::post('login', [AuthController::class, 'login'])->name('login');
 
 	Route::middleware('auth:sanctum')->group(function () {
-		Route::post('/logout', [AuthController::class, 'logout']);
-		Route::post('/chat', [ChatController::class, 'chat']);
-		Route::get('/chats', [ChatController::class, 'index']);
-		Route::get('/chats/{id}', [ChatController::class, 'show']);
-		Route::patch('/chats/{id}', [ChatController::class, 'update']);
-		Route::delete('/chats/{id}', [ChatController::class, 'destroy']);
+		Route::post('logout', [AuthController::class, 'logout']);
+		Route::post('chat', [ChatController::class, 'chat']);
+		Route::get('chats', [ChatController::class, 'index']);
+		Route::get('chats/{id}', [ChatController::class, 'show']);
+		Route::patch('chats/{id}', [ChatController::class, 'update']);
+		Route::delete('chats/{id}', [ChatController::class, 'destroy']);
 	});
 });
