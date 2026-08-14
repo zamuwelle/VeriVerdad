e6d321608a48e14a3bb6710cc3c9ac3463ac3b71
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-15 02:06:43 +0800

    feat: run postgres migration and queue worker on startup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+php artisan migrate --force --database=postgres_backup
 php artisan migrate --force
+php artisan queue:clear
 
 (while true; do
 	php artisan schedule:run
 	sleep 60
 done) &
+
+php artisan queue:work --sleep=3 --tries=3 &
 
 php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=8000
